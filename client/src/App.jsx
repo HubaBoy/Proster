e406b92b34b92c12d90dc84d8749dd7ee00e30a3
Gabi <86157167+GabrielPetrov@users.noreply.github.com>
2025-02-27 21:31:09 +0200

    Very basic post button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
- 
+function PostButton() {
+  return (
+    <button name = "postButton">
+      +
+    </button>
+  );
+}
 
+function App() {
   return (
     <>
       
+      <h1> Proster </h1>
+      <div id = "postField">
+        <label for = "postButton">Share your thoughts</label>
+        <PostButton />
+        {/* <input type="text" id="postContent" name = "What do you want to create today?"></input> */}
+      </div>
     </>
   )
 }
